aaea4eaa457b00c9ea0bdc706af35e5f59109ff0
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-07-02 10:46:15 +0100

    Chore: Build ts files

---
+++ Backend/build/controllers/product.controllers.js
@@ -0,0 +1,67 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const product_servicee_1 = __importDefault(require("../services/product.servicee"));
+const { create, getProductById } = new product_servicee_1.default();
+class ProductController {
+    createProduct(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const product = yield create(Object.assign(Object.assign({}, req.body), { userId: req.params.userId }));
+                return res.status(200)
+                    .send({
+                    success: true,
+                    message: "Product created successfully",
+                    product,
+                    blink: `/api/v1/action/${product._id}`
+                });
+            }
+            catch (error) {
+                return res.status(500)
+                    .send({
+                    success: false,
+                    message: `Error occured while ctreating product: ${error.message}`
+                });
+            }
+        });
+    }
+    getProductById(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const product = yield getProductById(req.params.productId);
+                if (!product) {
+                    return res.status(404)
+                        .send({
+                        success: false,
+                        message: "Product with the Id not found"
+                    });
+                }
+                return res.status(200)
+                    .send({
+                    success: true,
+                    message: "Product fetched successfully",
+                    product
+                });
+            }
+            catch (error) {
+                return res.status(500)
+                    .send({
+                    success: false,
+                    message: `Error occured while fetching product: ${error.message}`
+                });
+            }
+        });
+    }
+}
+exports.default = ProductController;
