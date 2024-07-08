1cb7c64347f6a60298865cf9780e12d5635819ef
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-07-08 15:09:09 +0100

    Fix conflict

--- Backend/src/controllers/action.controllers.ts
+++ Backend/src/controllers/action.controllers.ts
@@ -2,10 +2,8 @@ import { Request, Response } from "express";
 import ProductService from "../services/product.servicee";
 import { ACTIONS_CORS_HEADERS, ActionGetResponse, ActionPostRequest, ActionPostResponse } from "@solana/actions";
 import {
-  Authorized,
   clusterApiUrl,
   Connection,
-  Keypair,
   LAMPORTS_PER_SOL,
   PublicKey,
   sendAndConfirmTransaction,
@@ -14,29 +12,59 @@ import {
 } from "@solana/web3.js";
 
 const {
-  getProductById
+  getProductByQuery
 } = new ProductService();
 
 const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
-  "F6XAa9hcAp9D9soZAk4ea4wdkmX4CmrMEwGg33xD1Bs9", // SEL wallet
+  "F6XAa9hcAp9D9soZAk4ea4wdkmX4CmrMEwGg33xD1Bs9"
 );
 
 export default class ActionController {
   async getAction(req: Request, res: Response) {
     try {
-      const productId = req.originalUrl.split("/").pop();
-      const product = await getProductById(productId as unknown as string);
+      const baseHref = new URL(
+        `${req.protocol}://${req.get('host')}${req.originalUrl}`
+      ).toString();
 
-      const payload: ActionGetResponse = {
-        icon: product?.image as unknown as string,
-        label: `Buy Now (${product?.price} SOL)`,
-        description: `${product?.description}`,
-        title: `${product?.name}`,
+      const productName = req.params.name;
+      const product = await getProductByQuery({
+        name: productName
+      });
+
+      let payload: ActionGetResponse;
+      if (product?.payAnyPrice) {
+        payload = {
+          title: `${product?.name}`,
+          icon: product?.image as unknown as string,
+          description: `${product?.description}`,
+          label: `Buy Now`,
+          links: {
+            actions: [
+              {
+                label: `Buy Now`,
+                href: `${baseHref}?amount={amount}`,
+                parameters: [
+                  {
+                    name: "amount",
+                    label: "Enter a custom USD amount"
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      } else {
+        payload = {
+          icon: product?.image as unknown as string,
+          label: `Buy Now (${product?.price} SOL)`,
+          description: `${product?.description}`,
+          title: `${product?.name}`
+        }
       }
 
       res.set(ACTIONS_CORS_HEADERS);
 
-      return res.json(payload)
+      return res.json(payload);
 
     } catch (error: any) {
       return res.status(500)
@@ -49,10 +77,10 @@ export default class ActionController {
 
   async postAction(req: Request, res: Response) {
     try {
-      const productId = req.originalUrl.split("/").pop();
-      const product = await getProductById(productId as unknown as string);
-
-      const { toPubkey, sellerPubkey } = validatedQueryParams(req, product?.userId!);
+      const productName = req.params.name;
+      const product = await getProductByQuery({
+        name: productName
+      });
 
       const body: ActionPostRequest = req.body;
 
@@ -76,9 +104,21 @@ export default class ActionController {
         0 // Note: simple accounts that just store native SOL have `0` bytes of data
       );
 
-      if (product?.price! * LAMPORTS_PER_SOL < minimumBalance) {
-        throw `Account may not be rent exempt: ${toPubkey.toBase58()}`;
+      let price;
+      if (product?.payAnyPrice) {
+        price = parseFloat(req.query.amount as any);
+        if (price <= 0) throw new Error("amount is too small");
+      } else {
+        price = product?.price!;
       }
+
+      if (price * LAMPORTS_PER_SOL < minimumBalance) {
+        throw `account may not be rent exempt: ${DEFAULT_SOL_ADDRESS.toBase58()}`;
+      }
+
+      const sellerPubkey: PublicKey = new PublicKey(
+        product?.userId as string
+      );
 
       const transaction = new Transaction();
 
@@ -87,17 +127,17 @@ export default class ActionController {
         SystemProgram.transfer({
           fromPubkey: account,
           toPubkey: sellerPubkey,
-          lamports: Math.floor(product?.price! * LAMPORTS_PER_SOL * 0.9),
-        })
+          lamports: Math.floor(price * LAMPORTS_PER_SOL * 0.9),
+        }),
       );
 
       // Transfer 10% of the funds to the default SOL address
       transaction.add(
         SystemProgram.transfer({
           fromPubkey: account,
-          toPubkey: toPubkey,
-          lamports: Math.floor(product?.price! * LAMPORTS_PER_SOL * 0.1),
-        })
+          toPubkey: DEFAULT_SOL_ADDRESS,
+          lamports: Math.floor(price * LAMPORTS_PER_SOL * 0.1),
+        }),
       );
 
       // Set the end user as the fee payer
@@ -112,16 +152,27 @@ export default class ActionController {
       }).toString('base64');
 
       const payload: ActionPostResponse = {
-        transaction: serializedTransaction,
-        message: `You've successfully purchased ${product?.name} for ${product?.price} SOL 🎊`,
+        transaction: transaction.serialize({
+          requireAllSignatures: false,
+          verifySignatures: true,
+        }).toString('base64'),
+        message: `You've successfully purchased ${product?.name} for ${price} SOL 🎊`,
       };
+
+      if (product) {
+        product.quantity = product?.quantity - 1;
+        product.sales = product?.sales + 1;
+        product.revenue = product?.revenue + price;
+
+        await product.save();
+      }
 
       res.set(ACTIONS_CORS_HEADERS);
       res.status(200).json(payload);
 
       // Here is the new part where we wait for the transaction to be confirmed
       const signedTransaction = Transaction.from(Buffer.from(serializedTransaction, 'base64'));
-
+      
       // Sending and confirming the transaction
       const signature = await sendAndConfirmTransaction(connection, signedTransaction, []);
 
@@ -130,18 +181,8 @@ export default class ActionController {
       // If transaction is confirmed, log success and update database
       if (signature) {
         console.log("Transaction successful!");
-
         // Add your database update logic here
-        if (product) {
-          product.quantity = product?.quantity - 1;
-          product.sales = product?.sales + 1;
-          product.revenue = product?.revenue + price;
-
-          await product.save();
-        }
       }
-
-      return;
 
     } catch (error: any) {
       return res.status(500).send({
@@ -149,20 +190,5 @@ export default class ActionController {
         message: `Error: ${error.message}`,
       });
     }
-  } catch (err) {
-    throw "Invalid input query parameter: to";
   }
-
-  try {
-    if (req.query.seller) {
-      sellerPubkey = new PublicKey(req.query.seller as string);
-    }
-  } catch (err) {
-    throw "Invalid input query parameter: to";
-  }
-
-  return {
-    toPubkey,
-    sellerPubkey,
-  };
 }
