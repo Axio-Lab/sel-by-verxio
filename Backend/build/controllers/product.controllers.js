e551c82dc734c9d6a596e52742ddcf6247218f44
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-07-11 09:06:24 +0100

    Feat: Add endpoint to get user's products

--- Backend/build/controllers/product.controllers.js
+++ Backend/build/controllers/product.controllers.js
@@ -13,7 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const product_servicee_1 = __importDefault(require("../services/product.servicee"));
-const { create, getProductById, getProductByQuery } = new product_servicee_1.default();
+const { create, getProductById, getProductByQuery, getProducts } = new product_servicee_1.default();
 const deployedLink = "https://sel-by-verxio.onrender.com";
 const devnetBlink = "https://dial.to/devnet?action=solana-action:";
 class ProductController {
@@ -34,7 +34,7 @@ class ProductController {
                     success: true,
                     message: "Product created successfully",
                     product,
-                    blink: `${devnetBlink}${deployedLink}/api/v1/action/${encodeURIComponent(product.name)}`
+                    blink: `${deployedLink}/api/v1/action/${encodeURIComponent(product.name)}`
                 });
             }
             catch (error) {
@@ -62,7 +62,37 @@ class ProductController {
                     success: true,
                     message: "Product fetched successfully",
                     product,
-                    blink: `${devnetBlink}${deployedLink}/api/v1/action/${encodeURIComponent(product.name)}`
+                    blink: `${deployedLink}/api/v1/action/${encodeURIComponent(product.name)}`
+                });
+            }
+            catch (error) {
+                return res.status(500)
+                    .send({
+                    success: false,
+                    message: `Error occured while fetching product: ${error.message}`
+                });
+            }
+        });
+    }
+    getUserProduct(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const product = yield getProducts({ userId: req.params.id });
+                if (product.length === 0) {
+                    return res.status(404)
+                        .send({
+                        success: false,
+                        message: "Product with the userId not found"
+                    });
+                }
+                const products = product.map((one) => {
+                    return Object.assign(Object.assign({}, one), { blink: `${deployedLink}/api/v1/action/${encodeURIComponent(one.name)}` });
+                });
+                return res.status(200)
+                    .send({
+                    success: true,
+                    message: "Products fetched successfully",
+                    products
                 });
             }
             catch (error) {
--- Backend/src/controllers/product.controllers.ts
+++ Backend/src/controllers/product.controllers.ts
@@ -3,7 +3,8 @@ import ProductService from "../services/product.servicee";
 const {
     create,
     getProductById,
-    getProductByQuery
+    getProductByQuery,
+    getProducts
 } = new ProductService();
 const deployedLink = "https://sel-by-verxio.onrender.com";
 const devnetBlink = "https://dial.to/devnet?action=solana-action:";
@@ -12,12 +13,12 @@ export default class ProductController {
     async createProduct(req: Request, res: Response) {
         try {
             const foundProduct = await getProductByQuery({ name: req.body.name });
-            if(foundProduct) {
+            if (foundProduct) {
                 return res.status(409)
-                .send({
-                    success: false,
-                    message: "Product name already exists"
-                })
+                    .send({
+                        success: false,
+                        message: "Product name already exists"
+                    })
             }
             const product = await create({ ...req.body, userId: req.params.userId });
 
@@ -26,7 +27,7 @@ export default class ProductController {
                     success: true,
                     message: "Product created successfully",
                     product,
-                    blink: `${devnetBlink}${deployedLink}/api/v1/action/${encodeURIComponent(product.name)}`
+                    blink: `${deployedLink}/api/v1/action/${encodeURIComponent(product.name)}`
                 })
         } catch (error: any) {
             return res.status(500)
@@ -53,7 +54,40 @@ export default class ProductController {
                     success: true,
                     message: "Product fetched successfully",
                     product,
-                    blink: `${devnetBlink}${deployedLink}/api/v1/action/${encodeURIComponent(product.name)}`
+                    blink: `${deployedLink}/api/v1/action/${encodeURIComponent(product.name)}`
+                })
+        } catch (error: any) {
+            return res.status(500)
+                .send({
+                    success: false,
+                    message: `Error occured while fetching product: ${error.message}`
+                })
+        }
+    }
+
+    async getUserProduct(req: Request, res: Response) {
+        try {
+            const product = await getProducts({ userId: req.params.id });
+
+            if (product.length === 0) {
+                return res.status(404)
+                    .send({
+                        success: false,
+                        message: "Product with the userId not found"
+                    })
+            }
+
+            const products = product.map((one) => {
+                return {
+                    ...one,
+                    blink: `${deployedLink}/api/v1/action/${encodeURIComponent(one.name)}`
+                }
+            })
+            return res.status(200)
+                .send({
+                    success: true,
+                    message: "Products fetched successfully",
+                    products
                 })
         } catch (error: any) {
             return res.status(500)
