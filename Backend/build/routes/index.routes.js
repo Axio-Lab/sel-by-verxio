780d2256d75e4d705f5dbf19e2e42dc2902806cc
Adaobi Chuks-Aginam <chuksaginamada@yahoo.com>
2024-07-16 15:13:55 +0100

    Feat: Update endpoint naming for actions

--- Backend/build/routes/index.routes.js
+++ Backend/build/routes/index.routes.js
@@ -9,9 +9,9 @@ const action_routes_1 = __importDefault(require("./action.routes"));
 const doc_routes_1 = __importDefault(require("./doc.routes"));
 const constants_configs_1 = require("../configs/constants.configs");
 exports.default = (app) => {
+    app.use(`/`, action_routes_1.default);
     app.use(`${constants_configs_1.basePath}/profile`, profile_routes_1.default);
     app.use(`${constants_configs_1.basePath}/product`, product_routes_1.default);
-    app.use(`${constants_configs_1.basePath}/action`, action_routes_1.default);
     app.use(`${constants_configs_1.basePath}/docs`, doc_routes_1.default);
     app.use(`${constants_configs_1.basePath}/`, (_req, res) => {
         res.send("Welcome to Verxio API");
